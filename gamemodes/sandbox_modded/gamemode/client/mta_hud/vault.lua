e95792b06a5dc936e45c2fb78e05543a6e6b77f3
Earu <earu.derg@gmail.com>
2021-02-20 13:14:15 +0100

    this is the last one hopefully

--- gamemodes/sandbox_modded/gamemode/client/mta_hud/vault.lua
+++ gamemodes/sandbox_modded/gamemode/client/mta_hud/vault.lua
@@ -7,7 +7,7 @@ local mat = Matrix()
 
 surface.CreateFont("MTADrillFont", {
 	font = "Orbitron",
-	size = 26 * MTAHud.Config.ScrRatio,
+	size = 26 * MTA.HUD.Config.ScrRatio,
 	weight = 600,
 	shadow = false,
 	extended = true,
@@ -19,8 +19,8 @@ return function()
 
 	mat:SetField(2, 1, 0.05)
 
-	mat_vec.x = (-30 * MTAHud.Config.ScrRatio) + (MTAHud.Vars.LastTranslateY * 2)
-	mat_vec.y = (-100 * MTAHud.Config.ScrRatio) + (MTAHud.Vars.LastTranslateP * 3)
+	mat_vec.x = (-30 * MTA.HUD.Config.ScrRatio) + (MTA.HUD.Vars.LastTranslateY * 2)
+	mat_vec.y = (-100 * MTA.HUD.Config.ScrRatio) + (MTA.HUD.Vars.LastTranslateP * 3)
 
 	mat:SetTranslation(mat_vec)
 	cam.PushModelMatrix(mat)
@@ -28,22 +28,22 @@ return function()
 	surface.SetDrawColor(black_color)
 	local coef = ScrW() / 2560
 	local x, y = ScrW() - 400 * coef, ScrH() / 2
-	surface.DrawRect(x, y, 400 * coef, 55 * MTAHud.Config.ScrRatio)
+	surface.DrawRect(x, y, 400 * coef, 55 * MTA.HUD.Config.ScrRatio)
 
 	surface.SetDrawColor(orange_color)
-	surface.DrawOutlinedRect(x, y, 400 * coef, 55 * MTAHud.Config.ScrRatio, 2)
+	surface.DrawOutlinedRect(x, y, 400 * coef, 55 * MTA.HUD.Config.ScrRatio, 2)
 
 	surface.SetTextColor(white_color)
-	surface.SetTextPos(x + 10 * coef, y + (4 * MTAHud.Config.ScrRatio))
+	surface.SetTextPos(x + 10 * coef, y + (4 * MTA.HUD.Config.ScrRatio))
 	surface.SetFont("MTADrillFont")
 	surface.DrawText("/// DRILL PROGRESS ///")
 
 	local perc = vault:GetNWInt("DrillingProgress", 0)
 	local margin = ((300 * coef) * perc) / 100
 	surface.SetDrawColor(orange_color)
-	surface.DrawRect(x + 10 * coef, y + (35 * MTAHud.Config.ScrRatio), margin, 10)
+	surface.DrawRect(x + 10 * coef, y + (35 * MTA.HUD.Config.ScrRatio), margin, 10)
 
-	surface.SetTextPos(x + 15 * coef + margin, y + (25 * MTAHud.Config.ScrRatio))
+	surface.SetTextPos(x + 15 * coef + margin, y + (25 * MTA.HUD.Config.ScrRatio))
 	surface.DrawText(perc .. "%")
 
 	cam.PopModelMatrix()
