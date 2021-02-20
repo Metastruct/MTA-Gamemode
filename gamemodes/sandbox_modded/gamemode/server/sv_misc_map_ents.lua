2ea2b567baa2685a6e950a638c9f9b673125938d
Henka <henkan97@live.se>
2021-02-20 23:34:40 +0100

    Random supply crates around the map

--- gamemodes/sandbox_modded/gamemode/server/sv_misc_map_ents.lua
+++ gamemodes/sandbox_modded/gamemode/server/sv_misc_map_ents.lua
@@ -22,6 +22,98 @@ local randomChargers = {
 	{ pos = Vector(-5256, 2880, 5505), ang = Angle(0, 0, 0) },
 }
 
+local specialItems = {
+	"nextbot_chicken",
+	"npc_headcrab",
+	"npc_grenade_frag",
+	"npc_seagull",
+	"npc_pigeon",
+	"npc_crow",
+	"npc_cscanner",
+	"npc_manhack"
+}
+
+local itemList = {
+	--"item_dynamic_resupply",
+	"item_ammo_pistol",
+	"item_ammo_smg1",
+	"item_ammo_ar2",
+	"item_box_buckshot",
+	"item_ammo_357",
+	"item_ammo_crossbow",
+
+	"item_healthvial",
+	"item_battery",
+
+	"weapon_slam",
+	"weapon_frag",
+	"item_rpg_round",
+	"item_ammo_smg1_grenade",
+	"item_ammo_ar2_altfire",
+}
+
+local randomSupplyCrates = {
+	Vector(-393,  3003,  5418),
+	Vector(3300,  1537,  5435),
+	Vector(5989,  -2435, 5504),
+	Vector(-4239, -4473, 5296),
+	Vector(2794,  756,   5430),
+	Vector(4182,  2473,  5432),
+	Vector(4785,  7315,  5508),
+	Vector(3985,  6461,  5511),
+	Vector(2388,  10662, 5600),
+	Vector(1693,  8672,  5820),
+	Vector(-3025, 8582,  5816),
+	Vector(-4032, 7841,  5528),
+	Vector(-4573, 5352,  4784),
+	Vector(-4931, 3229,  4754),
+	Vector(-6552, 3877,  5120),
+	Vector(-6234, 1449,  5416),
+	Vector(-4410, -2216, 5428),
+	Vector(-4485, -2791, 5243),
+	Vector(-3370, 5662,  5496),
+}
+
+--10 to 15 minutes
+local respawnTime = {600, 900}
+local specialPercent = 5
+local boomPercent = 10
+
+local function CreateCrate(pos)
+	local ent = ents.Create("item_item_crate")
+
+	if math.random(0, 100) <= specialPercent then
+		local item = specialItems[math.random(1, #specialItems)]
+
+		ent:SetKeyValue("ItemClass", item)
+		ent:SetKeyValue("ItemCount", 1)
+	else
+		local item = itemList[math.random(1, #itemList)]
+
+		ent:SetKeyValue("ItemClass", item)
+		ent:SetKeyValue("ItemCount", 2)
+	end
+
+	-- separate calculation for boom, or it will explode everytime we get a special item lol
+	if math.random(0, 100) <= boomPercent then
+		ent:SetKeyValue("ExplodeDamage", 25)
+		ent:SetKeyValue("ExplodeRadius", 64)
+	end
+
+	ent:SetKeyValue("spawnflags", 8)
+	ent:SetPos(pos)
+	ent:Spawn()
+	ent.doRespawn = true
+
+	ent:CallOnRemove("crate_gone", function(self)
+		if self.doRespawn then
+			timer.Simple(math.Rand(respawnTime[1], respawnTime[2]), function()
+				MakeCrate(pos)
+			end)
+		end
+	end)
+end
+
 local function CreateCharger(pos, angle)
 	local ent = ents.Create("item_suitcharger")
 	ent:SetPos(pos)
@@ -38,6 +130,12 @@ local function CreateCharger(pos, angle)
 	return ent
 end
 
+local function SetupCrates()
+	for _, pos in ipairs(randomSupplyCrates) do
+		CreateCrate(pos)
+	end
+end
+
 local function SetupChargers()
 	for _, data in ipairs(hospitalChargers) do
 		CreateCharger(data.pos, data.ang)
@@ -49,6 +147,7 @@ local function SetupChargers()
 end
 
 hook.Add("InitPostEntity", tag, function()
+	SetupCrates()
 	SetupChargers()
 end)
 
