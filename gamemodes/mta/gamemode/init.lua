76fa4d21b58462c795fe9b1a150c7f70d74e43e2
Earu <earu.derg@gmail.com>
2022-01-20 15:19:28 +0100

    bruh

--- gamemodes/mta/gamemode/init.lua
+++ gamemodes/mta/gamemode/init.lua
@@ -53,7 +53,7 @@ include("server/sv_bank.lua")
 include("server/sv_combine_vault_skybox.lua")
 include("server/sv_apartments.lua")
 include("server/sv_wanders.lua")
-include("server/guilty_by_association.lua")
+include("server/sv_guilty_by_association.lua")
 
 function GM:EntityRemoved(ent)
 	-- Burning sounds are annoying.
